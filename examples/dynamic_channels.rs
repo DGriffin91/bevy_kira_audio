755dfbd92a712cd4abea00c263a73094642015aa
DGriffin91 <github@dgdigital.net>
2022-08-07 12:55:30 -0700

    pan with dynamic channel

--- examples/dynamic_channels.rs
+++ examples/dynamic_channels.rs
@@ -6,6 +6,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugin(AudioPlugin)
         .add_startup_system(play)
+        .add_system(pan)
         .run()
 }
 
@@ -13,4 +14,9 @@ fn play(asset_server: Res<AssetServer>, mut audio: ResMut<DynamicAudioChannels>)
     audio
         .create_channel("test")
         .play_looped(asset_server.load("sounds/loop.ogg"));
+}
+
+fn pan(time: Res<Time>, audio: Res<DynamicAudioChannels>) {
+    let t = time.seconds_since_startup() as f32;
+    audio.channel("test").set_panning(t.sin() * 0.5 + 0.5)
 }
